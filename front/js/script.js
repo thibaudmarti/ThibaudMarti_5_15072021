5d075592608483f1f7cddd71bdcb5bca45da446b
Thibaud <thibaud.marti@orange.fr>
2021-11-24 08:49:19 +0100

    Remove comment in script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,4 @@
 const urlAllKanaps = "http://localhost:3000/api/products";
-// import { variableTest } from "./object.js";
-// alert(variableTest);
 
 import { ModelKanap } from "./object.js";
 
@@ -16,45 +14,10 @@ async function getJsonFromApi(url) {
 /*
  * Convert a kanaps json array into html representation
  */
-// async function getHtmlKanaps(jsonKanaps) {
-//   htmlKanaps = "";
-
-//   // jsonKanaps.forEach((kanap) => {
-//     htmlKanaps += `<a href="./product.html?id=${._id}">
-//         <article>
-//           <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-//           <h3 class="productName">${kanap.name}</h3>
-//           <p class="productDescription">${kanap.description}</p>
-//         </article>
-//       </a>`;
-//   });
-
-//   return htmlKanaps;
-// }
-
-// function getHtmlKanaps(modelKanap) {
-//   //kanap._id => modelKanap._id
-
-//   htmlKanaps += `<a href="./product.html?id=${modelKanap._id}">
-//   <article>
-//     <img src="${modelKanap.imageUrl}" alt="${modelKanap.altTxt}">
-//     <h3 class="productName">${modelKanap.name}</h3>
-//     <p class="productDescription">${modelKanap.description}</p>
-//   </article>
-// </a>`;
-
-//   return htmlKanaps;
-// }
 
 function insertHtmlInPage(htmlToInsert, idParent) {
   document.getElementById(idParent).innerHTML = htmlToInsert;
 }
-
-// modelKanap => modelKanap.consoleLog();
-
-// function callConsoleLog(modelKanap) {
-//   modelKanap.consoleLog();
-// }
 
 async function DisplayKanaps() {
   getJsonFromApi(urlAllKanaps)
